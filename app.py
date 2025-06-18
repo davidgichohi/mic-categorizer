3ecd77765ceb55c21453d881fc3993cac99935d0
David Gichohi Maina <davidgmaina01@gmail.com>
2025-06-18 15:03:18 +0300

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -372,5 +372,11 @@ def index():
         color=color
     )
 
+#if __name__ == '__main__':
+    #app.run(debug=True)
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
+
